be8a1090dcaed6c7b6c69f57210f3d79d9036534
evgenii.prs <evgenii.prs@gmail.com>
2023-04-17 19:42:23 +0300

    Game runners

---
+++ src/telran/multithreading/runner/Race.java
@@ -0,0 +1,37 @@
+package telran.multithreading.runner;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Race {
+	
+	private int distance;
+	private int minSleep;
+	private int maxSleep;
+	
+	public AtomicInteger winner = new AtomicInteger(-1);
+	public Race(int distance, int minSleep, int maxSleep) {
+		this.distance = distance;
+		this.minSleep = minSleep;
+		this.maxSleep = maxSleep;
+	}
+	
+	public int getWinner() {
+		return winner.get();
+	}
+	
+	public int getDistance() {
+		return distance;
+	}
+	public int getMinSleep() {
+		return minSleep;
+	}
+	public int getMaxSleep() {
+		return maxSleep;
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/telran/multithreading/runner/Runner.java
@@ -0,0 +1,36 @@
+package telran.multithreading.runner;
+
+public class Runner extends Thread {
+	private Race race;
+	private int runnerId;
+	
+	public Runner (Race race, int runnerId) {
+		this.race = race;
+		this.runnerId = runnerId;
+	}
+	
+	@Override
+	public void run() {
+		int sleepRange = race.getMaxSleep() - race.getMinSleep() +1;
+		int minSleep = race.getMinSleep();
+		int distance = race.getDistance();
+		
+		for (int i = 0; i < distance; i++) {
+			try {
+				sleep((long) (minSleep + Math.random() * sleepRange));
+				
+			} catch (InterruptedException e) {
+				throw new IllegalStateException();
+			}
+			System.out.println(runnerId);
+			
+		}
+		race.winner.compareAndSet(-1, runnerId);
+		
+			
+		}
+	}
+	
+	
+
+
